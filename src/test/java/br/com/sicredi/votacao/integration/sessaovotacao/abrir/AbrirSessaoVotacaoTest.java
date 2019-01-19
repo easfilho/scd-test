d7658b606cf2dd79d02dfa82625bc2c1e912dac2
Edson Andrade <edson_andrade_filho@hotmail.com>
2019-01-19 00:20:40 -0200

    refactor: separados features de sessao de votação e voto

--- src/test/java/br/com/sicredi/votacao/integration/sessaovotacao/abrir/AbrirSessaoVotacaoTest.java
+++ src/test/java/br/com/sicredi/votacao/integration/sessaovotacao/abrir/AbrirSessaoVotacaoTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(Cucumber.class)
 @ContextConfiguration(loader = SpringBootContextLoader.class, classes = VotacaoApplication.class)
-@CucumberOptions(features = "src/test/resources/features/voto/AbrirSessaoVotacao.feature",
+@CucumberOptions(features = "src/test/resources/features/sessaovotacao/AbrirSessaoVotacao.feature",
         strict = true,
         glue = "br.com.sicredi.votacao.integration.sessaovotacao.abrir")
 public class AbrirSessaoVotacaoTest {
